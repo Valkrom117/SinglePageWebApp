a6a3da33ecdf0ef25ac9c10be9c21b8987640771
Valkrom117 <jvelas117@gmail.com>
2024-08-10 19:17:57 -0500

    Fixing Nothing found message

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -23,7 +23,7 @@ function NarrowedItemsDirective() {
 
 function NarrowItDownDirectiveController() {
     var list = this;
-    
+
 }
 
 NarrowItDownController.$inject = ['MenuSearchService'];
@@ -32,33 +32,38 @@ function NarrowItDownController(MenuSearchService) {
     list.foundItems = [];
     list.searchTerm = "";
 
-    list.logItems = function (searchTerm) {
-        var promise = MenuSearchService.getMatchedMenuItems();
-        let strTest = "chicken"
+
+    list.logItems = function () {
+        if (list.searchTerm) {
+            var promise = MenuSearchService.getMatchedMenuItems();
+            
+            promise.then(function (response) {
+                list.items = response.data;
+                for (let category in list.items) {
+                    for (let menuItem in list.items[category].menu_items) {
+
+                        if(list.items[category].menu_items[menuItem].description.toLowerCase().indexOf(list.searchTerm) !== -1) {
+                            list.foundItems.push(list.items[category].menu_items[menuItem]);
+                        };
+                    }
+                };
+            })
+            .catch(function (error) {
+                console.log(error)
+            });
+        };
         
-
-        promise.then(function (response) {
-            list.items = response.data;
-            for (let category in list.items) {
-                for (let menuItem in list.items[category].menu_items) {
-
-                    if(list.items[category].menu_items[menuItem].description.toLowerCase().indexOf(list.searchTerm) !== -1) {
-                        list.foundItems.push(list.items[category].menu_items[menuItem]);
-                    };
-                }
-            };
-            console.log('Found Items: ')
-            console.log(list.foundItems)
-            // narrowIt.filtered_items = foundItems;
-        })
-        .catch(function (error) {
-            console.log(error)
-        });
-    };
+        list.isEmptyList = function () {
+            if(list.foundItems.length !== 0 ){
+                return false;
+            }
+        return true;
+        };
+        console.log(list.isEmptyList)
+    }
     list.removeItem = function (itemIndex) {
         list.foundItems.splice(itemIndex, 1);
     }
-
 };
 
 MenuSearchService.$inject = ['$http', 'ApiBasePath']
